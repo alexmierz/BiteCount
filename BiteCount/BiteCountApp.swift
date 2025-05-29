9820d9f5641fd17598d4a15c641b1ca49609d13b
reassign <snowboardz125@gmail.com>
2025-05-29 18:59:17 -0400

    Initial Commit

---
+++ BiteCount/BiteCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteCountApp.swift
+//  BiteCount
+//
+//  Created by Alex Mierzejewski on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BiteCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
