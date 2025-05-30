c55e87fdb5d75801e4f713227bdc1fade5f22cbc
reassign <snowboardz125@gmail.com>
2025-05-30 19:03:40 -0400

    Set up Firebase

--- BiteCount/BiteCountApp.swift
+++ BiteCount/BiteCountApp.swift
@@ -5,10 +5,14 @@
 //  Created by Alex Mierzejewski on 5/29/25.
 //
 
-import SwiftUI
+import Firebase
 
 @main
 struct BiteCountApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
